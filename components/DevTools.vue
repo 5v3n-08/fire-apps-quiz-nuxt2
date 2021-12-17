e34eb2bcfd1b2b2cec21bf90b0a3cee2243dd677
Sven Stiels <sven.stiels@icloud.com>
2021-12-18 00:50:15 +0100

    chore(compontens): add apiUrl to DevTools

--- components/DevTools.vue
+++ components/DevTools.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="w-full flex justify-between fixed top-0 text-sm pt-1 text-gray-400 z-50"
+    class="w-full flex justify-between fixed top-0 text-sm pt-1 text-gray-200 z-50"
   >
     <div>
-      <!-- <div class="item">{{ process.env.BASE_URL }}</div> -->
+      <div class="item">{{ $config.apiUrl }}</div>
     </div>
     <div>
-      <div class="item xs:!inline">xs</div>
-      <div class="item sm:!inline">sm</div>
-      <div class="item md:!inline">md</div>
-      <div class="item lg:!inline">lg</div>
-      <div class="item xl:!inline">xl</div>
-      <div class="item 2xl:!inline">2xl</div>
+      <div class="item hidden xs:!inline">xs</div>
+      <div class="item hidden sm:!inline">sm</div>
+      <div class="item hidden md:!inline">md</div>
+      <div class="item hidden lg:!inline">lg</div>
+      <div class="item hidden xl:!inline">xl</div>
+      <div class="item hidden 2xl:!inline">2xl</div>
     </div>
   </div>
 </template>
@@ -21,13 +21,13 @@ import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
   mounted() {
-    console.log(process.env)
+    console.log(this.$config.apiUrl)
   },
 })
 </script>
 
 <style scoped>
 .item {
-  @apply border px-2 mx-1 border-black bg-black bg-opacity-30 rounded hidden;
+  @apply border px-2 mx-1 border-black bg-black bg-opacity-50 rounded;
 }
 </style>
