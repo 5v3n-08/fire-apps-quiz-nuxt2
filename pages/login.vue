6815ffb3f035173023a41f0c1b17c7cbd60ea51f
Sven Stiels <sven.stiels@icloud.com>
2021-12-18 00:51:02 +0100

    chore(pages): update login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <div class="bg-image h-full w-full">
     <!-- <div class=" /> -->
     <div class="flex justify-center items-center h-full w-full">
-      <v-card class="w-3/4 md:w-2/4 lg:w-2/6 pb-4">
+      <v-card class="w-3/4 md:w-2/4 lg:w-2/6 xl:1/6 max-w-lg pb-4">
         <v-card-title class="justify-center"> Anmeldung </v-card-title>
         <v-card-subtitle class="text-center">
           Bitte melde dich an um fortzufahren!
@@ -13,14 +13,17 @@
               outlined
               clearable
               label="E-Mail:"
-              hide-details="auto"
+              :hide-details="isTouched ? 'auto' : false"
+              type="mail"
+              :rules="[validateRequired, validateEmail]"
               v-model="email"
             ></v-text-field>
             <v-text-field
               outlined
               label="Passwort:"
-              hide-details="auto"
+              :hide-details="isTouched ? 'auto' : false"
               class="mt-2"
+              :rules="[validatePassword]"
               v-model="password"
               :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
               :type="showPassword ? 'text' : 'password'"
@@ -38,15 +41,35 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
+import { validateRequired, validateEmail } from '@/helpers/validation'
 
 export default defineComponent({
   layout: 'blank',
+  mounted() {
+    console.log()
+  },
   data() {
     return {
       showPassword: false,
+      isTouched: false,
       password: '',
       email: '',
     }
+  },
+  methods: {
+    onSignInClick() {},
+    validatePassword(value: string) {
+      return validateRequired(value, {
+        message: 'Bitte geben Sie ein Passwort ein!',
+      })
+    },
+    validateEmail,
+    validateRequired,
+  },
+  computed: {
+    isLoading() {
+      return this.$store.getters['global/isLoading'] as boolean
+    },
   },
 })
 </script>
